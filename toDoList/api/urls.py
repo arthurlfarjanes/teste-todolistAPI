94599ce3dcbb40c84419477f6ac895e05449199d
Arthur Lanzilotti Farjanes <arthurlfarjanes@gmail.com>
2024-10-16 14:40:30 -0300

    User and Task endpoints funcionando

--- toDoList/api/urls.py
+++ toDoList/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import get_users, create_user
+from .views import get_users, create_user, user_detail
+from .views import get_tasks, create_task, task_detail
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
     path('users/create', create_user, name='create_user'),
-    path('users/<int>:pk', create_user, name='create_user'),
+    path('users/<int:pk>', user_detail, name='user_detail'),
+    path('tasks/', get_tasks, name='get_tasks'),
+    path('tasks/create', create_task, name='create_task'),
+    path('tasks/<int:pk>', task_detail, name='task_detail'),
 ]
--- toDoList/api/views.py
+++ toDoList/api/views.py
@@ -30,12 +30,51 @@ def user_detail(request, pk):
         serializer = UserSerializer(user)
         return Response(serializer.data)
     
-    if request.method == 'PUT':
+    elif request.method == 'PUT':
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # Task Endpoits
+@api_view(['GET'])
+def get_tasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_task(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def task_detail(request, pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
